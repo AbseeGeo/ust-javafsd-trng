31e461d171d03edb70333cc03b12e760b0801a44
AbseeGeo <112387604+AbseeGeo@users.noreply.github.com>
2022-09-07 16:39:36 +0530

    Add files via upload

---
+++ SBA/SBA2_Q1_Employee_Abstract.java
@@ -0,0 +1,53 @@
+package SBA1;
+
+abstract class SBA2_Q1_Employee_Abstract1{
+	
+	abstract void calculateSalary(int hra,int ta, int ba);
+	abstract void fetchDepartment();
+}
+
+class Engineer extends SBA2_Q1_Employee_Abstract1{
+	
+	public void calculateSalary(int hra,int ta, int ba) {
+		float salary=hra+ta+ba;
+		System.out.println("Salary: "+salary);
+	}
+	
+	public void fetchDepartment() {
+		
+		System.out.println("Department: Mechanical");
+	}
+}
+
+class Doctor extends SBA2_Q1_Employee_Abstract1{
+	
+	public void calculateSalary(int hra,int ta, int ba) {
+		float salary=hra+ta+ba;
+		System.out.println("Salary: "+salary);
+	}
+	
+	public void fetchDepartment() {
+		
+		System.out.println("Department: Dental");
+	}
+}
+
+public class SBA2_Q1_Employee_Abstract {
+
+	public static void main(String[] args) {
+		
+		Engineer e = new Engineer();
+		System.out.println("Engineer");
+		System.out.println("----------------------");
+		e.calculateSalary(1000, 1000, 3000);
+		e.fetchDepartment();
+		
+		Doctor d = new Doctor();
+		System.out.println("\nDoctor");
+		System.out.println("----------------------");
+		d.calculateSalary(1000, 1000, 5000);
+		d.fetchDepartment();
+
+	}
+
+}
