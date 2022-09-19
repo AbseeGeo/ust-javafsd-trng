7dbe170cc42683cf11302e73d512911bf3655837
AbseeGeo <112387604+AbseeGeo@users.noreply.github.com>
2022-09-19 10:19:54 +0530

    Delete MYSQL directory

--- MYSQL/WEEK1/view_index.sql
+++
@@ -1,9 +0,0 @@
-use ustDB;
-SHOW TABLES;
-DESC EMPLOYEE;
-desc ust_table;
-create view  tb_view  as select name,state from ust_table;
-select * from tb_view;
-select *from ust_table;
-create index i_name on ust_table(name);
-show indexes from ust_table;
