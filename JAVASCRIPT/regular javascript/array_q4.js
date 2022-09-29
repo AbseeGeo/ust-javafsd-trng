266f94efee046e0848c1a34f09799f83582bbdde
AbseeGeo <112387604+AbseeGeo@users.noreply.github.com>
2022-09-29 11:43:59 +0530

    Add files via upload

---
+++ JAVASCRIPT/regular javascript/array_q4.js
@@ -0,0 +1,9 @@
+var week=new Array("sunday","monday","tuesday","wednesday","thursday","friday","saturday");
+function show(week)
+{
+    for(var i=0;i<week.length;i++)
+    {
+        console.log(week[i])
+    }
+}
+show(week);
---
+++ JAVASCRIPT/regular javascript/if_else_q1.js
@@ -0,0 +1,8 @@
+var name="absee";
+if(name=="absee")
+{
+    console.log("im absee");
+}
+else{
+    console.log("sorry im another person")
+}
