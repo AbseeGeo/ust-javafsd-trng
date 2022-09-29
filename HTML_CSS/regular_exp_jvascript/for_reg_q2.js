b24c3c66dfe6912724b9d86e86f0ddf2e4f7a2b8
AbseeGeo <112387604+AbseeGeo@users.noreply.github.com>
2022-09-29 16:33:20 +0530

    Add files via upload

---
+++ HTML_CSS/regular_exp_jvascript/for_reg_q2.js
@@ -0,0 +1,5 @@
+var i;
+for(i=1;i<=20;i++)
+{
+    console.log("9 x"+i+"=",9*i);
+}
---
+++ HTML_CSS/regular_exp_jvascript/switch_q3.js
@@ -0,0 +1,19 @@
+var num=14;
+switch(num){
+    case 0:{
+        console.log("travel");
+        break;
+    }
+    case 1:{
+        console.log("resturent");
+        break;
+    }
+    case 2:{
+        console.log("food");
+        break;
+    }
+    default:{
+        console.log("holiday");
+        break;
+    }
+}
