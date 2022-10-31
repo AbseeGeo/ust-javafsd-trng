89c927ee6c132506f53521748dcede52ece9cdb9
ustjavafsdb207 <Absee.Kollannoor@ust.com>
2022-10-31 17:36:58 +0530

    hai

--- JAVA/day5/List2.java
+++ JAVA/day5/List2.java
@@ -1,12 +1,21 @@
 package day5;
 import java.util.ArrayList;
+
+/**
+ * The type List 2.
+ */
 class List2 {
+	/**
+	 * Main.
+	 *
+	 * @param args the args
+	 */
 	public static void main(String[]args)
 	{
 		ArrayList<String> lang=new ArrayList<>();
 		lang.add("c");
 		lang.add("c++");
-		lang.add("java");
+		lang.add("hiiiii");
 		System.out.println("ArrayList:"+lang);
 		lang.set(2, "javascript");
 		System.out.println("modified arraylist"+lang);
