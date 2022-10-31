4d8387bdd762eb253154c39d5452586b6f6a53ab
ustjavafsdb207 <Absee.Kollannoor@ust.com>
2022-10-31 17:42:13 +0530

    hlooo

--- JAVA/day5/List2.java
+++ JAVA/day5/List2.java
@@ -14,7 +14,7 @@ class List2 {
 	{
 		ArrayList<String> lang=new ArrayList<>();
 		lang.add("c");
-		lang.add("c++");
+		lang.add("c++f");
 		lang.add("qwfv");
 		System.out.println("ArrayList:"+lang);
 		lang.set(2, "javascript");
